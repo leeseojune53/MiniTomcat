53edd59517b7ad5f73d6ba7f993a613f82e0fa2e
Leeseojune <sung07288346@gmail.com>
2025-02-18 23:44:46 +0900

    Project init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "org.example"
@@ -7,6 +8,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+allprojects {
+    apply(plugin = "com.diffplug.spotless")
+
+    spotless {
+        java {
+            palantirJavaFormat()
+        }
+    }
+
 }
 
 dependencies {
